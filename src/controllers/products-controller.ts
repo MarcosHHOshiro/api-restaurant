2616baa2cb3a382b34a3f4854710a7d186ff279d
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-23 13:59:47 -0400

    Creating the project structure

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+
+class ProductsController {
+    async index(req: Request, res: Response, next: NextFunction) {
+        try {
+
+            return res.json({ message: 'ok' })
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+export { ProductsController };
