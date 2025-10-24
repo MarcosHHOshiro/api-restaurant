5aeac44921a9fd5928198e3da924ba0a06df96e3
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-24 15:32:16 -0400

    Opening table

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { knex } from '@/database/knex';
+import z from 'zod';
 
 class TablesSessionsController {
     async index(req: Request, res: Response, next: NextFunction) {
         try {
-            return res.status(201).json({ message: "List of table sessions" });
+            const bodySchema = z.object({
+                table_id: z.number(),
+            });
+
+            const { table_id } = bodySchema.parse(req.body);
+
+            await knex<TablesSessionsRepository>('tables_sessions').insert({
+                table_id,
+                opened_at: knex.fn.now(),
+            });
+
+            return res.status(201).json({ message: "Table session created successfully" });
         }
         catch (error) {
             next(error);
--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -4,6 +4,6 @@ import { Router } from "express";
 const tablesSessionsRoutes = Router();
 const tablesSessionsController = new TablesSessionsController();
 
-tablesSessionsRoutes.get("/", tablesSessionsController.index);
+tablesSessionsRoutes.post("/", tablesSessionsController.index);
 
 export { tablesSessionsRoutes };
