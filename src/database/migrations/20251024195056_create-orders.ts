91704a854a0d6283d623dca746fab020c1f565b4
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-24 16:05:30 -0400

    orders migration

---
+++ src/database/migrations/20251024195056_create-orders.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("orders", (table) => {
+        table.increments("id").primary();
+        table.integer("table_session_id").references("id").inTable("tables_sessions").notNullable();
+        table.integer("product_id").references("id").inTable("products").notNullable();
+        table.integer("quantity").notNullable().defaultTo(1);
+        table.decimal("price").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("orders");
+}
+
