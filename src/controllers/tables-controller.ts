0a23571c0504685bef64ec83d1d6eb12f5c4baed
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-24 14:25:54 -0400

    creating get table route

---
+++ src/controllers/tables-controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { knex } from '@/database/knex';
+
+
+class TablesController {
+    async index(req: Request, res: Response, next: NextFunction) {
+        try {
+            const tables = await knex<TableRepository>("tables").select().orderBy("table_number", "asc");
+
+            return res.json(tables);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+export { TablesController };
