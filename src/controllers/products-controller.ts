59434f432c1c2abdda806c693d6647a55dd3e920
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-24 07:26:14 -0400

    validating data

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
+import { z } from "zod";
 
 class ProductsController {
     async index(req: Request, res: Response, next: NextFunction) {
         try {
 
             return res.json({ message: 'ok' })
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0, { message: "Price must be greater than zero" }),
+            })
+
+            const { name, price } = bodySchema.parse(req.body);
+ 
+            return res.status(201).json({ message: 'Product created' });
         } catch (error) {
             next(error);
         }
