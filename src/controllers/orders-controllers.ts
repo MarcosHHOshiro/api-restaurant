88b3e3e2dc94fa9873fe94be05b2dfac280e956d
Marcos <marcos.2h.oshiro@gmail.com>
2025-10-25 18:32:44 -0400

    creating order validations

--- src/controllers/orders-controllers.ts
+++ src/controllers/orders-controllers.ts
@@ -14,6 +14,22 @@ class OrdersController {
 
             const { table_session_id, product_id, quantity } = bodySchema.parse(req.body);
 
+            const session = await knex<TablesSessionsRepository>("table_sessions")
+                .where({ id: table_session_id })
+                .first();
+
+            if (!session) {
+                throw new AppError("Table session not found", 404);
+            }
+
+            const product = await knex<ProductRespository>("products")
+                .where({ id: product_id })
+                .first();
+
+            if (!product) {
+                throw new AppError("Product not found", 404);
+            }
+
             return res.status(201).json({ message: 'Order created' });
         }
         catch (error) {
